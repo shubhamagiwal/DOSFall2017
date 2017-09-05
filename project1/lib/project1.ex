0dd0860dcc4635c7b34d12c4d5f388bbde7608fe
Shubham Agiwal <shubhamagiwal@gmail.com>
2017-09-05 14:32:58 -0400

    Started with the sha256 generator function

--- project1/lib/project1.ex
+++ project1/lib/project1.ex
@@ -1,18 +1,19 @@
 defmodule Project1 do
+  use GenServer
 
   def ip_address_check(value) do
-
       case :inet.parse_address(to_charlist((value))) do
-            {:ok,ip}-> :true
-            {:error,:einval}-> :false
-           
+            {:ok,ip}-> {:true,to_string(value)}
+            {:error,:einval}-> {:false,to_string(value)}      
       end
-
   end 
 
+  def sha256Generator({status,value}) do
+       IO.puts :crypto.hash(:sha256,(to_string("shubhamagiwal92")<>SecureRandom.base64(8))) |> Base.encode16
+  end
+
   def main(args \\ []) do
-       ip_address_valid= to_string(args) |> ip_address_check #Check for the IP address validity
-       
+       ip_address_valid= to_string(args) |> ip_address_check |> sha256Generator #Check for the IP address validity and then pass it to sha256 generator       
     end
 
 end
