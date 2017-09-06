461a11c28dd7ecd1ca01cfc0109d4ddd0a4f5c9a
Shubham Agiwal <shubhamagiwal@gmail.com>
2017-09-05 20:11:28 -0400

    Started with the sha256 crypto function and its execution

--- project1/lib/project1.ex
+++ project1/lib/project1.ex
@@ -2,16 +2,35 @@ defmodule Project1 do
   use GenServer
 
   def ip_address_check(value) do
-      case :inet.parse_address(to_charlist((value))) do
+      case :inet.parse_ipv4strict_address(to_charlist((value))) do
             {:ok,ip}-> {:true,to_string(value)}
             {:error,:einval}-> {:false,to_string(value)}      
       end
   end 
 
   def sha256Generator({status,value}) do
-       IO.puts :crypto.hash(:sha256,(to_string("shubhamagiwal92")<>SecureRandom.base64(8))) |> Base.encode16
+       randomString=(to_string("shubhamagiwal92")<>SecureRandom.base64(8));
+       hash=:crypto.hash(:sha256,randomString) |> Base.encode16
+       if to_string(status)==to_string("false") do
+            status=String.slice(hash,0..String.to_integer(value)-1) |> check ;
+            printBitCoin(status,randomString,hash)
+            sha256Generator({false,value})
+       end
+       sha256Generator({false,value})
   end
 
+  def check(partOfHash) do
+      Enum.all?(String.graphemes(partOfHash),fn(x) -> x=="0" end);
+  end
+
+  def printBitCoin(status,randomString,hash) do
+      case status do
+           true->  IO.puts "#{randomString}  #{hash}"
+            _-> :ok
+      end 
+  end
+
+ 
   def main(args \\ []) do
        ip_address_valid= to_string(args) |> ip_address_check |> sha256Generator #Check for the IP address validity and then pass it to sha256 generator       
     end
