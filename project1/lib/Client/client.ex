80641f5ade5750991cfe9090646da260d6deab3b
karansharmaufl <sharma.karan9341@gmail.com>
2017-09-16 16:23:49 -0400

    Client mining bitcons

--- project1/lib/Client/client.ex
+++ project1/lib/Client/client.ex
@@ -12,11 +12,11 @@ defmodule Project1.Client do
         IO.inspect Node.connect(String.to_atom(to_string("server@"<>elem(tuple,1)))); 
         IO.inspect Node.list() 
         :global.sync()
-        #IO.inspect :global.whereis_name(:server);
         send(:global.whereis_name(:server),{:ok,self})
         IO.puts "Received from server"
         receive do
-            {:ok,k}-> IO.puts to_string(k)
+            {:ok,k}-> Project1.Worker.startWorker({Node.self,k})
+
         end
     end
 
