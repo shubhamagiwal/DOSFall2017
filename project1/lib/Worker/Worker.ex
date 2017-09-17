6586f725b8b720d60d71ffcb386e0b8a0f535363
karansharmaufl <sharma.karan9341@gmail.com>
2017-09-17 18:09:50 -0400

    Stable server and client printing

--- project1/lib/Worker/Worker.ex
+++ project1/lib/Worker/Worker.ex
@@ -2,14 +2,14 @@ defmodule Project1.Worker do
     use Supervisor
     
         def startWorker(tup_worker) do
-            IO.puts "Here"
-            IO.inspect tup_worker
+            
             Node.spawn(elem(tup_worker,0),fn -> Project1.Worker.start_link(tup_worker) end)
             
         end
 
         def start_link(tup_worker) do
-            Supervisor.start_link(__MODULE__, [tup_worker])
+            Supervisor.start_link(__MODULE__, [elem(tup_worker,0),elem(tup_worker,1)])
+            
         end
 
         def init(tup_worker) do
@@ -25,29 +25,25 @@ defmodule Project1.Worker do
         end
 
         def bitcoin_miner(tup_worker) do
-            IO.puts "454454545445"
-            IO.puts length(tup_worker)
-            #IO.inspect first(tup_worker)
-            #k=
-            #getBitCoins(k)
-            #bitcoin_miner(tup_worker)
+            getBitCoins(List.last(tup_worker),List.first(tup_worker))
+            bitcoin_miner(tup_worker)
         end
 
-        def getBitCoins(k) do
-            value=to_string(k)
+        def getBitCoins(k, str) do
+            value=k
             randomString=(to_string("shubhamagiwal92;")<>SecureRandom.base64(32));
             hash=:crypto.hash(:sha256,randomString) |> Base.encode16
             status=String.slice(hash,0..String.to_integer(value)-1) |> check ;
-            printBitCoin(status,randomString,hash)
+            printBitCoin(status,randomString,hash,str)
         end
 
           def check(partOfHash) do
               Enum.all?(String.graphemes(partOfHash),fn(x) -> x=="0" end);
           end
 
-          def printBitCoin(status,randomString,hash) do
+          def printBitCoin(status,randomString,hash,str) do
               case status do
-                   true->  IO.puts "#{randomString}  #{hash}"
+                   true->  IO.puts "#{randomString} #{str}  #{hash}"
                     _-> :ok
               end 
           end
