7a212635d8e500d9fa886c2e5030374af3925f0c
Shubham Agiwal <shubhamagiwal@gmail.com>
2017-09-18 14:21:18 -0400

    New Server Model

--- project1/lib/Server/Server.ex
+++ project1/lib/Server/Server.ex
@@ -1,39 +1,42 @@
 
 defmodule Project1.Server do
-        use Agent
-    
-        def start(tuple) do
-         serverName=String.to_atom(to_string("server@")<>elem(tuple,0))
-         IO.inspect serverName
-         {:ok,serverpid}=Node.start(serverName)
-         cookie=Application.get_env(:project1, :cookie)
-         Node.set_cookie(cookie)
-         IO.inspect self
-         :global.register_name(:server,self)
-         processes=String.to_integer(to_string(:erlang.system_info(:logical_processors)))*8
-         #1..processes |> Enum.map fn-> spawn(Project1.Worker.startWorker({Node.self,elem(tuple,1)})) end
-         spawn_processes(processes,0,elem(tuple,1))
-         loop(elem(tuple,0),elem(tuple,1))
-        end
-    
-         def loop(name,k) do
+        @workload 1000000
+        @worker 32
+      
+       def start(tuple) do
+        serverName=String.to_atom(to_string("server@")<>elem(tuple,0))
+        IO.inspect serverName
+        {:ok,serverpid}=Node.start(serverName)
+        cookie=Application.get_env(:project1, :cookie)
+        Node.set_cookie(cookie)
+        IO.inspect self
+        :global.register_name(:server,self)
+        send(self,{:ok,self})
+        IO.puts elem(tuple,1)
+        keep_server_alive(0,100000,elem(tuple,1))
+       end
 
-            receive do
-                {:ok,nodeName,pid} ->IO.inspect nodeName
-                                     pid1=Node.spawn(nodeName, fn-> Project1.Client.start_worker_client(name) end)
-                                     send(pid1,{:ok,nodeName,k,self})
-             end
+       def keep_server_alive(start_value,end_value,k) do
+           
+          receive do
+          {:ok,nodeName,nodePid} ->  IO.inspect nodeName
+                                     send(nodePid,{:ok,nodeName,k,self,start_value,end_value})
+                                     start_value=start_value+@workload
+                                     end_value=end_value+@workload
+                        
+          {:ok,serverPid} ->   IO.puts " iam here"
+                               send(serverPid,{:ok,serverPid,start_value,end_value,k})
+                               start_value=start_value+@workload
+                               end_value=end_value+@workload
 
-            loop(name,k)
-        end
+          {:ok,pid,startvalue,endvalue,k} ->
+                                            spawn(fn -> Project1.Worker.get_bit_coins(k,startvalue,endvalue,pid) end)
 
-        def spawn_processes(processes,times,k)do
-             if processes>times do
-                 IO.puts times
-                 spawn(fn-> Project1.Worker.startWorker({Node.self,k}) end) 
-                 spawn_processes(processes,times+1,k)
-             end
+          {:getnew,pid} -> spawn(fn -> Project1.Worker.get_bit_coins(k,start_value,end_value,pid) end)
+                       start_value=start_value+@workload
+                       end_value=end_value+@workload
+          end
 
-        end
-    
+           keep_server_alive(start_value,end_value,k)
+       end
     end
--- project1/lib/Worker/Worker.ex
+++ project1/lib/Worker/Worker.ex
@@ -1,51 +1,17 @@
 defmodule Project1.Worker do
-    use Supervisor
-    
-        # def startWorker(tup_worker) do
-        #     IO.puts "Hellow"
-        #     pid=Node.spawn(elem(tup_worker,0),fn -> Project1.Worker.start_link(elem(tup_worker,1)) end)
-        #     pid
-        # end
-
-        # def start_link(k) do
-        #     Supervisor.start_link(__MODULE__, [k])
-        #     Process.sleep(1_000_000)
-        # end
-
-        # def init(k) do
-        #     children = [
-        #       worker(Project1.Worker, [k], restart: :permanent, function: :spawn_bitcoin),
-        #       ]
-        #     supervise(children, strategy: :one_for_one)
-        # end
-
-         def startWorker(tup_worker) do
-            IO.puts "Hellow"
-            pid=Node.spawn(elem(tup_worker,0),fn -> Project1.Worker.spawn_bitcoin(elem(tup_worker,1)) end)
-            pid
-            donotexit
-        end
-
-        def  donotexit do
-            donotexit
-        end
-
-
-        def spawn_bitcoin(k) do
-             send(spawn(__MODULE__, :bitcoin_miner, [k]),{:ok})
-        end
-
-        def bitcoin_miner(k) do
-            getBitCoins(k)
-            bitcoin_miner(k)
-        end
-
-        def getBitCoins(k) do
-            value=to_string(k)
-            randomString=(to_string("shubhamagiwal92;")<>SecureRandom.base64(32));
-            hash=:crypto.hash(:sha256,randomString) |> Base.encode16
+        def get_bit_coins(k, start_value, end_value,pid) do
+            value = to_string(k)
+            if (start_value<end_value) do
+            random_string = to_string(to_string("shubhamagiwal92;")<>to_string(start_value))
+            hash=:crypto.hash(:sha256,random_string) |> Base.encode16
             status=String.slice(hash,0..String.to_integer(value)-1) |> check ;
-            printBitCoin(status,randomString,hash)
+            printBitCoin(status,random_string,hash)
+            start_value=start_value+1      
+            get_bit_coins(k, start_value, end_value,pid)      
+            else if(start_value>=end_value) do
+                send(pid,{:getnew,pid})
+            end   
+          end   
         end
 
           def check(partOfHash) do
@@ -58,9 +24,5 @@ defmodule Project1.Worker do
                     _-> :ok
               end 
           end
-
-         
-          
-
 
 end
