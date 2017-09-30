9041e2d525d5ea357a5662e68511638c168cc275
Shubham Agiwal <shubhamagiwal@gmail.com>
2017-09-30 17:26:34 -0400

    started with communication between processes

--- project2/lib/Server/server.ex
+++ project2/lib/Server/server.ex
@@ -1,13 +1,14 @@
 defmodule Project2.Server do
+use GenServer
 
     def start_server_node(server_tuple) do
         serverName=String.to_atom(to_string("server@")<>elem(server_tuple,0))
         {:ok,serveepmdrpid}=Node.start(serverName)
         cookie=Application.get_env(:project1, :cookie)
         Node.set_cookie(cookie)
-        :global.register_name(:server,self)
-       IO.inspect spawn_processes(String.to_integer(to_string(Enum.at(elem(server_tuple,1),0))),1,[])
-
+        list=spawn_processes(String.to_integer(to_string(Enum.at(elem(server_tuple,1),0))),1,[])
+        GenServer.cast(Enum.at(list,0),{:got,"Awesome"})
+        GenServer.cast(Enum.at(list,1),{:got,"Awesome2"})
     end
 
    def spawn_processes(numNodes,start_value,l) do
@@ -27,7 +28,12 @@ defmodule Project2.Server do
 
     #Server Side Implementation
     def init(args) do
-        {:ok,{}}
+        {:ok,%{}}
+    end
+
+    def handle_cast({:got,msg},state) do
+        IO.puts msg;
+        {:noreply,state}
     end
 
 end
