9467196e6a44dfe61dbdf37fb926d4e44e22974e
Shubham Agiwal <shubhamagiwal@gmail.com>
2017-09-30 20:36:28 -0400

    Initialisation of the full network topology is completed

--- project2/lib/Server/server.ex
+++ project2/lib/Server/server.ex
@@ -10,10 +10,11 @@ use GenServer
         IO.inspect list
         #GenServer.cast(Enum.at(list,0),{:got,"Awesome"})
         #GenServer.cast(Enum.at(list,1),{:got,"Awesome2"})
-        creating_topology(to_string(Enum.at(elem(server_tuple,1),1)),list)
+        creating_topology_for_each_actor(0,Enum.at(elem(server_tuple,1),1),list)
+        #creating_topology(to_string(Enum.at(elem(server_tuple,1),1)),list)
     end
 
-   def spawn_processes(numNodes,start_value,l) do
+    def spawn_processes(numNodes,start_value,l) do
              if(start_value<=numNodes) do
                 l=l++[start(start_value)]
                 start_value=start_value+1
@@ -22,12 +23,26 @@ use GenServer
              l
     end
 
-    def creating_topology(topology,list) do
+    def creating_topology_for_each_actor(start_value,topology,list) do
+            if(start_value<Enum.count(list)) do
+                list_of_neighbours=creating_topology(start_value,topology,list)
+                GenServer.cast(Enum.at(list,start_value),{:update,topology,list_of_neighbours})
+                start_value=start_value+1
+                creating_topology_for_each_actor(start_value,topology,list)
+            end
+
+
+    end
+
+   
+
+    def creating_topology(start_value,topology,list) do
 
          case topology do
 
-            "full" -> l = get_neighbours(0,"full",list,0,[])
-                      IO.inspect l
+            "full" -> 
+                      l = get_neighbours(start_value,"full",list,0,[])
+                      l
 
             "2D" -> IO.puts "Still to do"
 
@@ -68,18 +83,29 @@ use GenServer
 
    # Genserver processes start
    def start(start_value) do
-        {:ok,pid} = GenServer.start_link(__MODULE__,[],name: String.to_atom(to_string(start_value)))
-        pid
+        {:ok,pid} = GenServer.start_link(__MODULE__,:ok,name: String.to_atom(to_string(start_value)))
+        #pid
+        String.to_atom(to_string(start_value))
     end
 
     #Server Side Implementation
-    def init() do
+    def init(:ok) do
         # First is Rumor
-        {:ok,%{:count => 0,:list_of_neighbours =>[],:topology => nil }}
+        {:ok,%{}}
     end
 
-    def handle_cast({:got,msg},state) do
-        IO.puts msg;
+    def handle_cast({:update,topology,list_of_neighbours},state) do
+        #state[:topology]=topology
+        #%{state | :list_of_neighbours => list_of_neighbours}
+        #IO.inspect state
+        #IO.inspect list_of_neighbours
+        {_,state_list_neighbours}=Map.get_and_update(state,:list_of_neighbours, fn current_value -> {current_value,list_of_neighbours} end)
+        {_,state_list_count}=Map.get_and_update(state,:count, fn current_value -> {current_value,0} end)
+        {_,state_list_topology}=Map.get_and_update(state,:topology, fn current_value -> {current_value,topology} end)
+        state=Map.merge(state,state_list_neighbours)
+        state=Map.merge(state,state_list_count)
+        state=Map.merge(state,state_list_topology)
+        IO.inspect state
         {:noreply,state}
     end
 
