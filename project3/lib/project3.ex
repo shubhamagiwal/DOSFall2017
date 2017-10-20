c97e36666c81886eaeb3f8f3bc6b4f4eb12f4dd0
shubhamagiwal <shubhamagiwal@gmail.com>
2017-10-20 17:33:43 -0400

    Added project3 initial commit

---
+++ project3/lib/project3.ex
@@ -0,0 +1,18 @@
+defmodule Project3 do
+  @moduledoc """
+  Documentation for Project3.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Project3.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
