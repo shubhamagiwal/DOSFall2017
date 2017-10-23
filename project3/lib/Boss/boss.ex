55f657f1485ee4f15ce6129e88c2516558bdaaae
shubhamagiwal <shubhamagiwal@gmail.com>
2017-10-22 21:58:39 -0400

    Bonus with fix is done

--- project3/lib/Boss/boss.ex
+++ project3/lib/Boss/boss.ex
@@ -26,6 +26,19 @@ def handle_cast({:update_boss_state,numRequest,numNodes,list_of_nodes_with_pids}
 
 end
 
+def handle_cast({:update_boss_state,dead_list,alive_list},state) do
+        {_,state_alivelist}=Map.get_and_update(state,:alive_list, fn current_value -> {current_value,alive_list} end)
+        state=Map.merge(state,state_alivelist)
+        {_,state_deadlist}=Map.get_and_update(state,:dead_list, fn current_value -> {current_value,alive_list} end)
+        state=Map.merge(state,state_deadlist)
+        {_,state_numNodes}=Map.get_and_update(state,:numNodes, fn current_value -> {current_value,length(alive_list)} end)
+        state=Map.merge(state,state_numNodes)
+
+        #IO.inspect state
+
+        {:noreply,state}
+end
+
 def handle_cast({:delivered,hopcount,process,hash},state) do
      #IO.puts "#{inspect process} #{inspect hash} total-requested= #{state[:total_request_received]} hopcount= #{state[:hop_count]}"
      #IO.puts "#{inspect state[:total_request_received]} #{inspect (state[:numRequest] * state[:numNodes])}"
@@ -35,10 +48,17 @@ def handle_cast({:delivered,hopcount,process,hash},state) do
      {_,state_total_request_received}=Map.get_and_update(state,:total_request_received, fn current_value -> {current_value,current_value+1} end)
      state=Map.merge(state,state_total_request_received)
 
-     if(state[:total_request_received]>=(state[:numRequest] * state[:numNodes])) do
+    # IO.inspect state
+
+     if(state[:total_request_received]>=(state[:numRequest] * state[:numNodes]) and (state[:numRequest] * state[:numNodes])>0) do
         average_hop_count=state[:hop_count]/(state[:numRequest] * state[:numNodes])
         IO.puts "The average hop count is #{inspect average_hop_count}"
         Process.exit(self(),:normal)
+     end
+
+     if( (state[:numRequest] * state[:numNodes])==0) do
+         IO.puts "The average hop count is 0"
+          Process.exit(self(),:normal)
      end
 
     {:noreply,state}
@@ -54,6 +74,8 @@ end
 
         numNodes=String.to_integer(to_string(Enum.at(elem(server_tuple,1),0)))
         numRequest=String.to_integer(to_string(Enum.at(elem(server_tuple,1),1)))
+        pct = String.to_integer(to_string(Enum.at(elem(server_tuple,1),2)))
+        nodes_to_kill = round((pct*numNodes)/100) # Number of nodes to be killed
 
         #Spawn Nodes for given number of nodes
         #Here we are not going to run for more than a 100 thousand nodes  to restricting the node space to 100000
@@ -62,30 +84,59 @@ end
         list_of_nodes_with_pids=spawn_nodes(numNodes,1,[],[],Enum.to_list(number))
         # Returns a tuple with hash, node id and pid for the every node in ascending order of their node ids to start with the topology
         list_of_nodes_with_pids=Enum.sort(list_of_nodes_with_pids,fn(x,y) -> elem(x,2)<elem(y,2)  end)
-        #IO.inspect list_of_nodes_with_pids
-
+    
         #Initialising the boss
-        GenServer.cast(Boss_Server,{:update_boss_state,numRequest,numNodes,list_of_nodes_with_pids})
+         GenServer.cast(Boss_Server,{:update_boss_state,numRequest,numNodes,list_of_nodes_with_pids})
         #Building the topology
         build_topology(list_of_nodes_with_pids,numRequest)
-        send_to_node(list_of_nodes_with_pids, numNodes, 0)
+
+        #Kill Nodes
+        killed_nodes=kill_nodes(list_of_nodes_with_pids, nodes_to_kill, [])
+        alive_nodes = list_of_nodes_with_pids -- killed_nodes
+
+        #updating the boss state
+        killed_nodes=Enum.sort(killed_nodes,fn(x,y) -> elem(x,2)<elem(y,2)  end)
+        alive_nodes=Enum.sort(alive_nodes,fn(x,y) -> elem(x,2)<elem(y,2)  end)
+        GenServer.cast(Boss_Server,{:update_boss_state,killed_nodes,alive_nodes})
+                
+        send_to_node(alive_nodes,length(alive_nodes), 0,killed_nodes,alive_nodes,list_of_nodes_with_pids)
         
 
  end
 
- def send_to_node(list_of_nodes, num_nodes, counter) do
-        if(counter < num_nodes) do
-        random_node = Enum.random(list_of_nodes) # Get a randome node
-        list_of_nodes = list_of_nodes -- [random_node] # Update list
-        counter = counter+1 # Update counter
-        node_id = elem(random_node,2)
-        count = 0
-        node_list_count = num_nodes
-        process_id = elem(random_node,0)
-        hash = elem(random_node,1)
-        #IO.inspect random_node
-        GenServer.cast(process_id,{:receive_request_to_cast,node_id,count,node_list_count,process_id,hash,@b})
-        send_to_node(list_of_nodes, num_nodes, counter)
+ def kill_nodes(list, nodes_count, killed_nodes) do
+        if( nodes_count > 0) do
+                random_node = Enum.random(list)
+                pid = elem(random_node,0)
+                Process.exit(pid, :normal)
+                list = list -- [random_node]
+                killed_nodes = killed_nodes ++ [random_node]
+                nodes_count = nodes_count-1 
+                killed_nodes=kill_nodes(list, nodes_count, killed_nodes)
+        end
+        killed_nodes
+ end
+
+ def send_to_node(list_of_nodes, num_nodes, counter,killed_nodes,alive_nodes,list_nodes_pid) do
+        #IO.inspect length(alive_nodes)
+        if(counter < num_nodes and length(alive_nodes)>1)do
+                 random_node = Enum.random(list_of_nodes) # Get a randome node
+                 list_of_nodes = list_of_nodes -- [random_node] # Update list
+                 counter = counter+1 # Update counter
+                 node_id = elem(random_node,2)
+                 count = 0
+                 node_list_count = num_nodes
+                 process_id = elem(random_node,0)
+                 hash = elem(random_node,1)
+                 GenServer.cast(process_id,{:receive_request_to_cast,node_id,count,node_list_count,process_id,hash,@b,killed_nodes,alive_nodes,list_nodes_pid})
+                 send_to_node(list_of_nodes, num_nodes, counter,killed_nodes,alive_nodes,list_nodes_pid)
+        else if( length(alive_nodes)==1) do
+               GenServer.cast(elem(Enum.at(alive_nodes,0),0),{:sendHopCountDirect,@b})
+        else if(length(alive_nodes)==0) do
+               #IO.puts "I am here"
+               GenServer.cast(Boss_Server,{:delivered,0,nil,nil})
+             end
+           end
         end
  end
 
--- project3/lib/Node/node.ex
+++ project3/lib/Node/node.ex
@@ -1,7 +1,5 @@
 defmodule Project3.Node do
 use GenServer
-@random_string "shubhamagiwal92"
-@nodeLength 10
     
     #Generate Node process
     def start(random_node_id,b) do
@@ -44,42 +42,105 @@ use GenServer
         {:noreply,state}
     end
 
-    def handle_cast({:receive_request_to_cast,nodeId,count,node_list_count,process_id,hash,b},state) do
-       
+    def handle_cast({:receive_request_to_cast,nodeId,count,node_list_count,process_id,hash,b,killed_nodes,alive_nodes,list_nodes_pid},state) do
         if(count<state[:num_request]) do
         #Generate Key and cast again with count incremented
         #Generated the random key and cast
         #random_number = to_string(:rand.uniform(node_list_count))
+
         key=:crypto.hash(:sha,Project3.LibFunctions.randomizer(128,true))|> Base.encode16 |> Convertat.from_base(16) |> Convertat.to_base(b+1)
         #IO.puts "#{inspect self()} is #{count} has key #{inspect key}"
         #key=String.slice(key,0..@nodeLength)
         #IO.puts "#{key} is the the key and #{hash} is the hash"
-        GenServer.cast(self(),{:route,key,hash,b,0})
+        GenServer.cast(self(),{:route,key,hash,b,0,killed_nodes,alive_nodes,list_nodes_pid})
         #Generated the random key and cast ended 
 
         #Recast to itself
         #Process.sleep(1_000)
         #IO.puts "key = #{key} send for #{inspect self()}"
-        GenServer.cast(self(),{:receive_request_to_cast,nodeId,count,node_list_count,process_id,hash,b})
+        GenServer.cast(self(),{:receive_request_to_cast,nodeId,count,node_list_count,process_id,hash,b,killed_nodes,alive_nodes,list_nodes_pid})
         #Recast to itself done
         end
         {:noreply,state}
 
     end
 
-    def handle_cast({:route,key,hashOfNode,b,hops},state) do
+    def handle_cast({:updateleafset,larger_leaf_set,smaller_leaf_set},state) do
+        {_,state_larger_leaf_set}=Map.get_and_update(state,:larger_leaf_set, fn current_value -> {current_value,larger_leaf_set} end)
+        state=Map.merge(state,state_larger_leaf_set)
+        {_,state_smaller_leaf_set}=Map.get_and_update(state,:smaller_leaf_set, fn current_value -> {current_value,smaller_leaf_set} end)
+        state=Map.merge(state,state_smaller_leaf_set)
+        {:noreply,state}
+    end
 
+    def handle_cast({:updateneighbourset,neigbhour_set},state) do
+        {_,state_neighbor_set}=Map.get_and_update(state,:neighbor_set, fn current_value -> {current_value,neigbhour_set} end)
+        state=Map.merge(state, state_neighbor_set)
+        {:noreply,state}
+    end
+
+    def handle_cast({:updateroutingtable,routing_table},state) do
+        {_,state_neighbor_set}=Map.get_and_update(state,:routing_table, fn current_value -> {current_value,routing_table} end)
+        state=Map.merge(state, state_neighbor_set)
+        {:noreply,state}
+    end
+
+    def handle_cast({:sendHopCountDirect,b},state) do
+
+            dummy_call_to_boss(0,state[:num_request],b)
+        
+        {:noreply,state}
+    end
+
+    def dummy_call_to_boss(index,numRequest,b) do
+            if(index<numRequest)do
+                        GenServer.cast(Boss_Server,{:delivered,0,self(),:crypto.hash(:sha,Project3.LibFunctions.randomizer(128,true))|> Base.encode16 |> Convertat.from_base(16) |> Convertat.to_base(b+1)})
+                        dummy_call_to_boss(index+1,numRequest,b)
+            end
+    end
+
+    def handle_cast({:route,key,hashOfNode,b,hops,killed_nodes,alive_nodes,list_nodes_pid},state) do
         #Combine the leafsets small and big with sorting
+        #Update the leafsets with new enteries
+       
         leafsets=state[:larger_leaf_set]++state[:smaller_leaf_set]
-        leafsets=Enum.sort(leafsets,fn(x,y) -> elem(x,1)<elem(y,1)  end)
+        length_1=length(leafsets--alive_nodes)
+        length_2=length(state[:neighbor_set]--alive_nodes)
+        #IO.puts "value #{inspect length_1} "
+        #GEt new random leafs
+       
+            if(length_1>0) do
+                #IO.puts "Old Leafset "
+                leafsets=get_new_Leafs(leafsets,killed_nodes,alive_nodes,list_nodes_pid)
+                #IO.inspect leafsets
+                if(length(leafsets)>0) do
+                    chunks=Enum.chunk(leafsets,round(length(leafsets)/2), round(length(leafsets)/2), [])
+                    GenServer.cast(self(),{:updateleafset,Enum.at(chunks,0),Enum.at(chunks,1)})
+                else
+                    chunks=leafsets
+                    GenServer.cast(self(),{:updateleafset,chunks,[]})
+                end
+            end
 
+            if(length_2>0) do
+                neighbor_set=get_new_neighbourhoodSet(state[:neighbor_set],killed_nodes,alive_nodes,list_nodes_pid)
+                GenServer.cast(self(),{:updateneighbourset,neighbor_set})
+            end
+
+            routing_table=update_routing_table(state[:routing_table],0,0,state[:number_rows],state[:number_columns],killed_nodes,alive_nodes,list_nodes_pid,%{})
+            #IO.puts "#{inspect routing_table} "
+            GenServer.cast(self(),{:updateroutingtable,routing_table})
+
+        #Remove the deadnodes if any Done
+        leafsets=Enum.sort(leafsets,fn(x,y) -> elem(x,1)<elem(y,1)  end)
+        #Update the small and larger leaf set
         lowersLeafSetValue=elem(Enum.at(leafsets,0),1)
         higherLeafSetValue=elem(Enum.at(leafsets,length(leafsets)-1),1)
+        #Remove the deadnodes and 
 
+             
         if(key>=lowersLeafSetValue and key<=higherLeafSetValue) do
             # Value is in the neighbourhood set
-            # IO.puts "I am in the neigbhourser #{inspect self()}"
-            #IO.puts "Delivered #{key} in leafset"
             GenServer.cast(Boss_Server,{:delivered,hops+1,self(),key})
         else
             #Find the longest matching prefix which the key and hashNode
@@ -97,7 +158,7 @@ use GenServer
             if(elem(nearest_neighbour,2)==-1) do
                 # No Nearest longest+1 prefix match found
                 # Combine Leaf small set,right set and routing table set
-                combineSet=leafsets++get_routing_table_row(state[:routing_table],state[:number_rows],state[:number_columns],[],0,0)
+                combineSet=leafsets++get_routing_table_row(state[:routing_table],state[:number_rows],state[:number_columns],[],0,0)++state[:neighbor_set]
                 #IO.inspect combineSet
                 nearest_node=no_nearest_longestplus1_prefix_node(combineSet,key,hashOfNode,row)
                 #IO.puts "Nearest node for key #{inspect key} is #{inspect nearest_node} "
@@ -109,13 +170,13 @@ use GenServer
                 else
                     #Cast to the given nearest neighbour
                     #IO.inspect "Casting"
-                    GenServer.cast(elem(nearest_node,0),{:route,key,elem(nearest_node,1),b,hops+1})
+                    GenServer.cast(elem(nearest_node,0),{:route,key,elem(nearest_node,1),b,hops+1,killed_nodes,alive_nodes,list_nodes_pid})
                 end
             else
                 # Yes Nearest longest+1 prefix match found
                 # Send it to the given node with the incremented hop count
                 #IO.inspect "Casting"
-                GenServer.cast(elem(nearest_neighbour,0),{:route,key,elem(nearest_neighbour,1),b,hops+1})
+                GenServer.cast(elem(nearest_neighbour,0),{:route,key,elem(nearest_neighbour,1),b,hops+1,killed_nodes,alive_nodes,list_nodes_pid})
                 #IO.puts "Send"
 
             end
@@ -345,7 +406,6 @@ use GenServer
             routing_table=route_columns(node_list,numberOfColumns,numberOfRows,row_index,column_index+1,hashOfNode,old_substring,routing_table)
         end
         routing_table
-
     end
 
     def find_element_in_list_match_substring(node_list,substring,hashOfNode) do
@@ -368,5 +428,118 @@ use GenServer
         end
         value
     end
+
+    def get_new_Leafs(leafsets,killed_nodes,alive_nodes,list_nodes_pid) do
+            newLeafsets=leafsets--killed_nodes
+            #IO.puts "#{inspect self} #{inspect length(newLeafsets)} #{inspect length(leafsets)}" 
+            if(length(newLeafsets)==length(leafsets)) do
+            #No change in the leafset
+            else
+                deadLeafSetNodes=leafsets--alive_nodes
+                # IO.puts "#{inspect self} #{inspect deadLeafSetNodes} dead" 
+               newLeafsets=get_new_Leafs_due_to_dead(newLeafsets,length(deadLeafSetNodes),alive_nodes)
+             end
+
+            #O.puts "#{inspect self} #{inspect length(newLeafsets)} #{inspect length(leafsets)} after" 
+            newLeafsets 
+    end 
+
+    def get_new_Leafs_due_to_dead(newLeafsets,index,alive_nodes) do
+
+        if(index>0 and length(alive_nodes)>1) do
+             newLeafsets=newLeafsets++[get_random_node(alive_nodes,newLeafsets,{})]
+             newLeafsets=get_new_Leafs_due_to_dead(newLeafsets,index-1,alive_nodes)
+        else    
+            newLeafsets=newLeafsets++[get_random_node(alive_nodes,newLeafsets,{})]
+        end
+        newLeafsets
+    end
+
+    def get_random_node(alive_nodes,newLeafsets,value)do
+        value={-1,-1,-1}
+        random_node=Enum.random(alive_nodes)
+        #IO.puts "#{inspect self} #{inspect random_node}"
+        if(length(alive_nodes)==1) do
+            value=random_node
+        else if(Enum.member?(newLeafsets,random_node)) do
+            value=get_random_node(alive_nodes,newLeafsets,value)
+        else    
+            value=random_node
+            end
+        end
+        value
+    end
+
+
+     def get_new_neighbourhoodSet(neighbourset,killed_nodes,alive_nodes,list_nodes_pid) do
+            newneighbourset=neighbourset--killed_nodes
+            if(length(newneighbourset)==length(neighbourset)) do
+            #No change in the leafset
+            else
+                deadneighbourhoodset=neighbourset--alive_nodes
+                Enum.each(deadneighbourhoodset,fn(x)-> 
+                     newneighbourset=newneighbourset++[get_random_node(alive_nodes,newneighbourset,{})]
+                 end)
+             end
+            newneighbourset 
+    end 
+
+    def update_routing_table(routing_table,row_index,column_index,
+                             numberRows,numberColumns,killed_nodes,alive_nodes,list_nodes_pid,
+                             newrouting_table) do
+            if(row_index<numberRows) do
+                  #IO.puts "#{inspect routing_table} #{inspect routing_table[row_index]} is here"
+                newrouting_table1=update_routing_table_column(routing_table,row_index,column_index,
+                             numberRows,numberColumns,killed_nodes,alive_nodes,list_nodes_pid,
+                             newrouting_table)
+
+                newrouting_table= Map.merge(newrouting_table1,routing_table)
+                routing_table=newrouting_table
+
+                newrouting_table2=update_routing_table(routing_table,row_index+1,column_index,
+                             numberRows,numberColumns,killed_nodes,alive_nodes,list_nodes_pid,
+                             newrouting_table)
+
+                newrouting_table= Map.merge(newrouting_table2,routing_table) 
+                routing_table=newrouting_table     
+            end
+
+            newrouting_table
+    end
+
+    def update_routing_table_column(routing_table,row_index,column_index,
+                             numberRows,numberColumns,killed_nodes,alive_nodes,list_nodes_pid,
+                             newrouting_table) do
+
+            if(column_index<numberColumns) do
+              
+                tuple=routing_table[row_index][column_index]
+               
+                if(elem(tuple,2)!=-1 and !Process.alive?(elem(tuple,0))) do
+                    newtuplevalue=get_alive_nodes_for_routing_table(alive_nodes,String.slice(elem(tuple,1),row_index+1),killed_nodes,0)
+                    {_,updated_routing_table}=Map.get_and_update(routing_table,row_index,fn current_value -> {current_value,%{column_index => newtuplevalue}} end)
+                    routing_table=Map.merge(routing_table,updated_routing_table)   
+                    newrouting_table=routing_table
+                    routing_table=update_routing_table_column(routing_table,row_index,column_index+1,
+                                                              numberRows,numberColumns,killed_nodes,alive_nodes,list_nodes_pid,
+                                                              newrouting_table)
+                end
+            end
+
+            newrouting_table
+
+    end
+
+    def get_alive_nodes_for_routing_table(alive_nodes,start_match,dead_nodes,index) do
+        value={-1,-1,-1}
+        if(String.starts_with?(elem(Enum.at(alive_nodes,index),1),start_match) or length(alive_nodes)==1) do
+            value=Enum.at(alive_nodes,index)
+        else
+            value=get_alive_nodes_for_routing_table(alive_nodes,start_match,dead_nodes,index)
+        end
+        value
+    end
+
+
 
 end
