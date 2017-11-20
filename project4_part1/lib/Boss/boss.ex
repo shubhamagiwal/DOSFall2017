6fd5a4bc1e3ea1b1ad36d9de843344eb15aaf0cd
shubhamagiwal <shubhamagiwal@gmail.com>
2017-11-20 16:10:31 -0500

    One tweet per user creation and saving it on the server is done for n nodes

--- project4_part1/lib/Boss/boss.ex
+++ project4_part1/lib/Boss/boss.ex
@@ -1,6 +1,29 @@
 defmodule Project4Part1.Boss do
 use GenServer
 
+def init(:ok) do
+        {:ok,%{:tweet => [], :tweet_text=> [] ,:name_node => [],:hashTag => [] }}
+end
+
+def handle_cast({:got_tweet,random_tweet,random_tweet_text,random_hashTag,name_node},state)do
+
+
+        {_,state_random_tweet}=Map.get_and_update(state,:tweet, fn current_value -> {current_value,current_value++[random_tweet]} end)
+        state=Map.merge(state,state_random_tweet)
+
+        {_,state_random_tweet_text}=Map.get_and_update(state,:tweet_text, fn current_value -> {current_value,current_value++[random_tweet_text]} end)
+        state=Map.merge(state,state_random_tweet_text)
+
+        {_,state_random_tweet_tweeter}=Map.get_and_update(state,:name_node, fn current_value -> {current_value,current_value++[name_node]} end)
+        state=Map.merge(state,state_random_tweet_tweeter)
+
+        {_,state_random_tweet_hashTag}=Map.get_and_update(state,:hashTag, fn current_value -> {current_value,current_value++[random_hashTag]} end)
+        state=Map.merge(state,state_random_tweet_hashTag)
+        
+        IO.inspect state
+        {:noreply,state}
+end
+
  def start_boss(server_tuple) do
         serverName=String.to_atom(to_string("server@")<>elem(server_tuple,0))
         {:ok,_}=Node.start(serverName)
@@ -11,21 +34,17 @@ use GenServer
         numNodes=String.to_integer(to_string(Enum.at(elem(server_tuple,1),0)))
 
         # Spawn the given numNodes which is registering an account with a password
-        spawn_nodes(numNodes,0,[])
-        
-
+        spawn_nodes(numNodes,1,[])
  end
 
  def spawn_nodes(numNodes,start_value,l) do
              if(start_value<=numNodes) do
                 l=l++[Project4Part1.Node.start(start_value)]
-                name_of_node=String.to_atom("tweeter@user"<>to_string(start_value))
-                GenServer.cast(name_of_node,{:check})
+                #name_of_node=String.to_atom("tweeter@user"<>to_string(start_value))
+                #GenServer.cast(name_of_node,{:check})
                 start_value=start_value+1
                 l=spawn_nodes(numNodes,start_value,l)
              end
              l
-end
-
- 
+ end
 end
--- project4_part1/lib/Node/node.ex
+++ project4_part1/lib/Node/node.ex
@@ -6,13 +6,29 @@ use GenServer
         name_of_node=String.to_atom("tweeter@user"<>to_string(id_tweeter))
         password=Project4Part1.LibFunctions.randomizer(8,true)
         {:ok,_} = GenServer.start_link(__MODULE__,password,name: name_of_node)
+        GenServer.cast(name_of_node,{:tweet,name_of_node})
         {name_of_node}
     end
 
     #Server Side Implementation
     def init(args) do  
         #IO.puts "#{inspect self} #{inspect args}" 
-        {:ok,%{:password => args}}
+        {:ok,%{:password => args, :isFreshUser => true}}
+    end
+
+    def handle_cast({:update_user_status,status},state)do
+          {_,state_isFreshUser}=Map.get_and_update(state,:isFreshUser, fn current_value -> {current_value,true} end)
+          state=Map.merge(state,state_isFreshUser)
+          {:noreply,state}
+    end
+
+    def handle_cast({:tweet,name_node},state)do
+        random_tweet_text=Project4Part1.LibFunctions.randomizer(32,:downcase)
+        random_hashTag=" #"<>Project4Part1.LibFunctions.randomizer(8,true)
+        random_tweet=random_tweet_text<>random_hashTag
+        IO.puts "#{inspect name_node} tweet: #{inspect random_tweet}"
+        GenServer.cast(Boss_Server,{:got_tweet,random_tweet,random_tweet_text,random_hashTag,name_node})
+        {:noreply,state}
     end
 
     def handle_cast({:check},state)do
