635b8eb7dae9ae1260e12899db37e06c12162c31
shubhamagiwal <shubhamagiwal@gmail.com>
2017-11-20 20:27:39 -0500

    Server side engine in progress

--- project4_part1/lib/Boss/boss.ex
+++ project4_part1/lib/Boss/boss.ex
@@ -3,32 +3,68 @@ use GenServer
 @numTweets 2
 
 def init(:ok) do
-        {:ok,%{:tweet => [], :tweet_usr=> [] ,:name_node => [],:hashTag => [] }}
+        {:ok,%{:name_node => [],:hashTag => [],:tweet_user=>[],:tweet=>[]}}
 end
 
-def handle_cast({:created_user,name_node},state)do
+def handle_cast({:created_user,name_node,password,isFreshUser,isLoggedIn},state)do
 
-      {_,state_random_tweet_tweeter}=Map.get_and_update(state,:name_node, fn current_value -> {current_value,current_value++[name_node]} end)
-      state=Map.merge(state,state_random_tweet_tweeter)
+      process_map=%{:name_node => nil, :password => nil, :is_fresh_user=>nil,:is_logged_in => nil, :tweets =>[] , :subscribers => [], :subscribees => [], :hashTags => []  }
 
+      {_,state_name_node}=Map.get_and_update(process_map,:name_node, fn current_value -> {current_value,name_node} end)
+      process_map=Map.merge(process_map,state_name_node)
+
+      {_,state_password}=Map.get_and_update(process_map,:password, fn current_value -> {current_value,password} end)
+      process_map=Map.merge(process_map,state_password)
+
+      {_,state_tweets}=Map.get_and_update(process_map,:tweets, fn current_value -> {current_value,[]} end)
+      process_map=Map.merge(process_map,state_tweets)
+
+       {_,state_subscribers}=Map.get_and_update(process_map,:subscribers, fn current_value -> {current_value,[]} end)
+      process_map=Map.merge(process_map,state_subscribers)
+
+      {_,state_subscribees}=Map.get_and_update(process_map,:subscribees, fn current_value -> {current_value,[]} end)
+      process_map=Map.merge(process_map,state_subscribees)
+
+      {_,state_hashtags}=Map.get_and_update(process_map,:hashTags, fn current_value -> {current_value,[]} end)
+      process_map=Map.merge(process_map,state_hashtags)
+      
+      {_,state_random_tweet}=Map.get_and_update(state,:tweet_user, fn current_value -> {current_value,current_value++[process_map]} end)
+      state=Map.merge(state,state_random_tweet)
+
+      {_,state_random_name_node}=Map.get_and_update(state,:name_node, fn current_value -> {current_value,current_value++[name_node]} end)
+      state=Map.merge(state,state_random_name_node)
+      
+      #IO.inspect state
       {:noreply,state}
-
 end
 
-def handle_cast({:got_tweet,random_tweet,random_tweet_text,random_hashTag,name_node},state)do
+def handle_cast({:got_tweet,random_tweet,random_hashTag,name_node,tweeter_id},state)do
 
-        {_,state_random_tweet}=Map.get_and_update(state,:tweet, fn current_value -> {current_value,current_value++[random_tweet]} end)
-        state=Map.merge(state,state_random_tweet)
+        tweeter_user_state=Enum.at(state[:tweet_user],tweeter_id-1)
 
-        {_,state_random_tweet_usr}=Map.get_and_update(state,:tweet_usr, fn current_value -> {current_value,current_value++[name_node]} end)
-        state=Map.merge(state,state_random_tweet_usr)
+        {_,state_random_tweet}=Map.get_and_update(tweeter_user_state,:tweets, fn current_value -> {current_value,current_value++[random_tweet]} end)
+        tweeter_user_state=Map.merge(tweeter_user_state,state_random_tweet)
 
-        {_,state_random_tweet_hashTag}=Map.get_and_update(state,:hashTag, fn current_value -> {current_value,current_value++[random_hashTag]} end)
-        state=Map.merge(state,state_random_tweet_hashTag)
+        modified_tweet_user_list= List.replace_at(state[:tweet_user],tweeter_id-1,tweeter_user_state)
 
-        IO.inspect state
+        {_,state_updated_tweet_user_list}=Map.get_and_update(state,:tweet_user, fn current_value -> {current_value,modified_tweet_user_list} end)
+        state=Map.merge(state,state_updated_tweet_user_list)
+
+        {_,state_updated_tweet_hashTag}=Map.get_and_update(state,:hashTag, fn current_value -> {current_value,current_value++[random_hashTag]} end)
+        state=Map.merge(state,state_updated_tweet_hashTag)
+
+        {_,state_updated_tweet_tweet}=Map.get_and_update(state,:tweet, fn current_value -> {current_value,current_value++[random_tweet]} end)
+        state=Map.merge(state,state_updated_tweet_tweet)
+
+        #IO.inspect state
         {:noreply,state}
 end
+
+def handle_cast({:update_user_status},state)do
+
+         IO.inspect state
+         {:noreply,state}
+    end
 
  def start_boss(server_tuple) do
         serverName=String.to_atom(to_string("server@")<>elem(server_tuple,0))
@@ -40,29 +76,31 @@ end
         numNodes=String.to_integer(to_string(Enum.at(elem(server_tuple,1),0)))
 
         # Spawn the given numNodes which is registering an account with a password
-        spawn_nodes(numNodes,1,[])
+        spawn_nodes(numNodes,1)
+
+        Process.sleep(1_00)
+
+         GenServer.cast(Boss_Server,{:update_user_status})
  end
 
- def spawn_nodes(numNodes,start_value,l) do
+ def spawn_nodes(numNodes,start_value) do
+
              if(start_value<=numNodes) do
                 name_of_node=Project4Part1.Node.start(start_value)
-                l=l++[name_of_node]
-                #name_of_node=String.to_atom("tweeter@user"<>to_string(start_value))
-                #GenServer.cast(name_of_node,{:check})
-                create_tweet_for_user(@numTweets,name_of_node,1)
+                create_tweet_for_user(@numTweets,name_of_node,1,start_value)
                 start_value=start_value+1
-                l=spawn_nodes(numNodes,start_value,l)
+                l=spawn_nodes(numNodes,start_value)
              end
-             l
+           
   end
 
-    def create_tweet_for_user(numtweets,name_of_user,start_value)do
+    def create_tweet_for_user(numtweets,name_of_user,start_value,id_tweeter)do
 
         if(start_value<=numtweets)do
-            GenServer.cast(name_of_user,{:tweet,name_of_user})
+            GenServer.cast(name_of_user,{:tweet,name_of_user,id_tweeter})
             start_value=start_value+1
-            create_tweet_for_user(numtweets,name_of_user,start_value)
+            create_tweet_for_user(numtweets,name_of_user,start_value,id_tweeter)
         end
-        GenServer.cast(name_of_user,{:update_user_status,false})
     end
+
 end
--- project4_part1/lib/Node/node.ex
+++ project4_part1/lib/Node/node.ex
@@ -6,34 +6,40 @@ use GenServer
         name_of_node=String.to_atom("tweeter@user"<>to_string(id_tweeter))
         password=Project4Part1.LibFunctions.randomizer(8,true)
         {:ok,_} = GenServer.start_link(__MODULE__,password,name: name_of_node)
-        GenServer.cast(Boss_Server,{:created_user,name_of_node})
+        isFreshUser=true
+        isLoggedIn=true
+        GenServer.cast(Boss_Server,{:created_user,name_of_node,password,isFreshUser,isLoggedIn})
         name_of_node
     end
 
     #Server Side Implementation
     def init(args) do  
-        #IO.puts "#{inspect self} #{inspect args}" 
-        {:ok,%{:password => args, :isFreshUser => true}}
+        {:ok,%{:is_logged_in=>true,:is_fresh_user=>true}}
     end
 
-    def handle_cast({:update_user_status,status},state)do
-          {_,state_isFreshUser}=Map.get_and_update(state,:isFreshUser, fn current_value -> {current_value,true} end)
-          state=Map.merge(state,state_isFreshUser)
+    def handle_cast({:tweet,name_node,tweeter_id},state)do
+        if(state[:is_logged_in]==true) do
+            random_tweet_text=Project4Part1.LibFunctions.randomizer(32,:downcase)
+            random_hashTag="#"<>Project4Part1.LibFunctions.randomizer(8,true)
+            random_tweet=random_tweet_text<>random_hashTag
+            IO.puts "#{inspect name_node} tweet: #{inspect random_tweet}"
+            GenServer.cast(Boss_Server,{:got_tweet,random_tweet,random_hashTag,name_node,tweeter_id})
+        else
+            #Do Nothing
+        end
+        {:noreply,state}
+    end
+
+     def handle_cast({:login},state)do
+          {_,state_isLoggedIn}=Map.get_and_update(state,:is_logged_in, fn current_value -> {current_value,true} end)
+          state=Map.merge(state,state_isLoggedIn)
           {:noreply,state}
     end
 
-    def handle_cast({:tweet,name_node},state)do
-        random_tweet_text=Project4Part1.LibFunctions.randomizer(32,:downcase)
-        random_hashTag=" #"<>Project4Part1.LibFunctions.randomizer(8,true)
-        random_tweet=random_tweet_text<>random_hashTag
-        IO.puts "#{inspect name_node} tweet: #{inspect random_tweet}"
-        GenServer.cast(Boss_Server,{:got_tweet,random_tweet,random_tweet_text,random_hashTag,name_node})
-        {:noreply,state}
-    end
-
-    def handle_cast({:check},state)do
-        IO.puts "I am here in #{inspect self()} #{inspect state[:password]}"
-        {:noreply,state}
+    def handle_cast({:logout},state)do
+          {_,state_isLoggedIn}=Map.get_and_update(state,:is_logged_in, fn current_value -> {current_value,false} end)
+          state=Map.merge(state,state_isLoggedIn)
+          {:noreply,state}
     end
 
 end
